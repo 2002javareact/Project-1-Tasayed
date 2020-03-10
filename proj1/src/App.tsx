30c603cd0a73edd8dd49ce39c0374d441d4a3c97
tasayed <taqrim.sayed@gmail.com>
2020-03-10 15:38:28 -0400

    submodule/componentstart

--- proj1/src/App.tsx
+++ proj1/src/App.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { MessageComponent } from './components/MessageComponent';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Route path='/' render={(props) => <MessageComponent history={props.history} match={props.match} location={props.location} message={'An example of the render design pattern for components'} />} />
+        
+      </Router>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+      
       </header>
     </div>
   );
